8376b0c1f6accc19d89d5ffe244e493346b9b878
laks1408 <laksplaysmc@gmail.com>
2024-02-27 14:50:46 +0700

    hyperlink map, score, pp (gabisa)

--- osutil.py
+++ osutil.py
@@ -13,29 +13,43 @@ def response_osu(message):
     if p_message[0] == 'r':
 
         embed = discord.Embed()
-        embed.title = "Recent Play"
+        embed.title = f'Recent osu! play for {p_message[1]}'
         
-        
+        #User ID / Name
         id = api.user(p_message[1]).id
         scores = api.user_scores(id, "recent", include_fails=True, mode=None, limit=1)
-        
         recent = scores[0]
+
+        #Acc
         acc = recent.accuracy*100
         acc_rounded= round(acc,2)
 
-        title = recent.beatmapset.title
+        #PP (masih ga bisa)
+        pp = recent.pp
+        if pp is float:
+            pp = round(pp,2)
+        elif pp is None:
+            pp = 'No pp'
 
+        #Score
+        score = recent.score
+
+        #Map details
+        title = recent.beatmapset.title
+        beatmapid = recent.beatmap.id
+
+        #Rank emoji gaje
         rank = str(recent.rank).split('.')[1]
         rank_emoji = get_rank_emoji(rank)
         
-
-
+        #Beatmap image
         image = f'https://b.ppy.sh/thumb/{recent.beatmapset.id}.jpg'
         embed.set_thumbnail(url=image)
 
-        embed.description = f'Played by {p_message[1]}, {title} \n Accuracy: {rank_emoji}{acc_rounded} %'
+        embed.description = f'[{title}](https://osu.ppy.sh/b/{beatmapid}) \n Score: {score} \n Accuracy: {rank_emoji}{acc_rounded}% \n PP: {pp}\n'
 
         return embed
+
     
 def get_rank_emoji(rank: str) -> discord.Emoji:
     if rank == 'A':
@@ -52,3 +66,5 @@ def get_rank_emoji(rank: str) -> discord.Emoji:
         pass
     elif rank == 'SH':
         pass
+
+ 
